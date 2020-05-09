c53bb6aaab788b2563fb397d73eacf4244cb5adc
Kostiantyn <ostap99.2002@gmail.com>
2020-05-09 08:05:56 +0300

    made paginator

---
+++ coaches.py
@@ -0,0 +1,29 @@
+from telegram_bot_pagination import InlineKeyboardPaginator
+
+
+coaches_list = {1: ("Остапенко Константин", "+380675729936" ),
+                2: ("Второй тренер", "номер телефона"),
+                3: ("Третий тренер", "номер телефона"),
+                4: ("Чётвёртый тренер", "номер телефона"),
+                5: ("Пятый тренер", "номер телефона"),
+                6: ("Шестой тренер", "номер телефона"),
+}
+
+
+class MyPaginator(InlineKeyboardPaginator):
+    previous_page_label = '<'
+    current_page_label = '-{}-'
+    next_page_label = '>'
+
+    paginator = InlineKeyboardPaginator(
+        len(coaches_list),
+        current_page=1,
+        data_pattern='coaches_list#{page}'
+    )
+
+
+# bot.send_message(
+#     chat_id,
+#     text,
+#     reply_markup=paginator.markup,
+# )
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 import phrases as ph
 from keyboards import main_keyboard
 from keyboards import schedule_keyboard
-from telegram_bot_pagination import InlineKeyboardPagination
+import coaches as pg
 
 
 bot = telebot.TeleBot(config.TOKEN)
@@ -37,9 +37,29 @@ def callback_inline(call):
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'coaches')
-def callback_inline(call):
-    bot.send_message(call.message.chat.id, ph.COACHES_MESSAGE)
+def coaches_list_callback(call):
+    pg.page = int()
+    bot.delete_message(
+        call.message.chat.id,
+        call.message.message_id
+    )
 
+
+def send_coaches_list(message, page=1):
+    paginator = pg.InlineKeyboardPaginator(
+        len(pg.coaches_list),
+        current_page=1,
+        data_pattern='coaches_list#{page}'
+    )
+
+    bot.send_message(
+        message.chat.id,
+        pg.coaches_list[page-1],
+        reply_markup=paginator.markup,
+        parse_mode='Markdown'
+    )
+    bot.send_message(message.chat.id, ph.COACHES_MESSAGE)
+    send_coaches_list(message, page)
 
 @bot.callback_query_handler(func=lambda call: call.data == 'ask')
 def callback_inline(call):
