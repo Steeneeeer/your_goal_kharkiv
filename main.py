32508376e5a188f4e4690a331d38bab98699f3fc
HolidayMan <lesha.pitenko791@gmail.com>
2020-06-05 11:52:12 +0300

    fast fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import phrases as ph
 from keyboards import main_keyboard
 from keyboards import schedule_keyboard, schedule_button, address_button, prices_button, ice_info_button, coaches_button, ask_button
 import coaches as pg
-from message_sending import *
+# from message_sending import *
 
 from bot_init import bot
 
